745c48c19d9436bfa69d82b2f4f62a81ff098127
Ben Le <benjamin.d.le@protonmail.com>
2024-02-03 15:06:58 -0600

    Add speaker shooting methods

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,16 +4,34 @@
 
 package frc.robot;
 
+import edu.wpi.first.units.Distance;
+import static edu.wpi.first.units.Units.*;
+import edu.wpi.first.units.Measure;
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+  }
+
+  // TODO: shooter constants
+  public static class ShooterConstants {
+    public static final int feederMotorCANID = 9;
+    public static final int kickerMotorCANID = 10;
+    public static final int shooterRightCANID = 11;
+    public static final int shooterLeftCANID = 12;
+
+    // TODO: fix import error
+    public static final Measure<Distance> shooterWheelDiameter = units.Units.Inches.of(4.0);
   }
 }
--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -2,13 +2,92 @@ package frc.robot.subsystems.shooter;
 
 import com.revrobotics.CANSparkFlex;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
-
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.Velocity;
+import edu.wpi.first.units.Angle;
+import edu.wpi.first.units.Distance;
+import static edu.wpi.first.units.Units.*;
 
 public class Shooter extends SubsystemBase {
+    private CANSparkMax m_feederMotor;
     private CANSparkFlex m_kickerMotor;
-    private CANSparkFlex m_leftShooterMotor;
-    private CANSparkFlex m_rightShooterMotor;
-    private CANSparkMax m_backIntakeMotor;
+    private CANSparkFlex m_shooterLeftMotor;
+    private CANSparkFlex m_shooterRightMotor;
+
+    private SparkPIDController m_shooterLeftPIDController;
+    private SparkPIDController m_shooterRightPIDController;
+
+    // TODO: constructor
+    public Shooter() {
+        m_feederMotor = new CANSparkMax(Constants.ShooterConstants.feederMotorCANID, MotorType.kBrushless);
+        m_kickerMotor = new CANSparkFlex(Constants.ShooterConstants.kickerMotorCANID, MotorType.kBrushless);
+        m_shooterLeftMotor = new CANSparkFlex(Constants.ShooterConstants.shooterLeftCANID, MotorType.kBrushless);
+        m_shooterRightMotor = new CANSparkFlex(Constants.ShooterConstants.shooterRightCANID, MotorType.kBrushless);
+
+        m_feederMotor.restoreFactoryDefaults();
+        m_kickerMotor.restoreFactoryDefaults();
+        m_shooterLeftMotor .restoreFactoryDefaults();
+        m_shooterRightMotor.restoreFactoryDefaults();
+
+        m_shooterLeftPIDController = m_shooterLeftMotor.getPIDController();
+        m_shooterRightPIDController = m_shooterRightMotor.getPIDController();
+
+        // Set PID values
+    }
+
+    // TODO: spinFeeder
+    private void spinFeeder(double speed)
+    {
+       m_feederMotor.set(speed);
+    }
+
+    // TODO: spinKicker
+    private void spinKicker() {
+
+    }
+
+    // TODO: spinShooter
+    private void spinShooterAngular(Measure<Velocity<Angle>> leftSpeed, Measure<Velocity<Angle>> rightSpeed) {
+        m_shooterLeftPIDController.setReference(leftSpeed.in(RPM), CANSparkFlex.ControlType.kVelocity);
+        m_shooterRightPIDController.setReference(rightSpeed.in(RPM), CANSparkFlex.ControlType.kVelocity);
+    }
+
+    private void spinShooterLinear(Measure<Velocity<Distance>> leftLinear, Measure<Velocity<Distance>> rightLinear) {
+        // TODO
+    }
+
+    // TODO: stop method
+    public void stopAll() {
+      m_feederMotor.stopMotor();
+      m_kickerMotor.stopMotor();
+      m_shooterLeftMotor.stopMotor();
+      m_shooterRightMotor.stopMotor();
+    }
+
+    // TODO: speaker shot method
+    public void setShootSpeed(Measure<Velocity<Distance>> launchSpeed, Measure<Velocity<Angle>> rotationalSpeed)
+    {
+        
+    }
+    
+
+    // TODO: getIsAtShooterSpeed method
+    private boolean getIsAtShooterSpeed() {
+        // TODO
+        
+        return true;
+    }
+
+
+    // TODO: speakerFire method
+    public void speakerFire() {
+        
+    }
+
 }
