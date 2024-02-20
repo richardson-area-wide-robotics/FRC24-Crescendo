af27e5dd3cc130fde7e55a5ddb954eb026b5c287
Ben Le <benjamin.d.le@protonmail.com>
2024-02-20 13:09:23 -0600

    Add Climber Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,11 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkBase.IdleMode;
+import edu.wpi.first.units.Angle;
+import edu.wpi.first.units.Measure;
+import static edu.wpi.first.units.Units.*;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -15,5 +20,43 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+  }
+
+  public static class ClimberConstants {
+    public static final int kClimberLeftCANID = 0;
+    public static final int kClimberRightCANID = 1;
+
+    public static final IdleMode kClimberIdleMode = IdleMode.kBrake;
+
+    public static final boolean kClimberLeftInverted = false;
+    public static final boolean kClimberRightInverted = true;
+
+    public static final int kClimberCurrentLimit = 40;
+
+    public static final boolean kClimberForwardSoftLimitEnabled = true;
+    public static final boolean kClimberReverseSoftLimitEnabled = true;
+
+    public static final float kClimberLeftForwardSoftLimit = 0;
+    public static final float kClimberRightForwardSoftLimit = 0;
+
+    public static final float kClimberLeftReverseSoftLimit = 0;
+    public static final float kClimberRightReverseSoftLimit = 0;
+
+    public static final double kLeftP = 0.1;
+    public static final double kLeftI = 0.0;
+    public static final double kLeftD = 0.0;
+
+    public static final double kRightP = 0.1;
+    public static final double kRightI = 0.0;
+    public static final double kRightD = 0.0;
+    
+    public static final double kClimbSpeed = 0.5;
+    public static final double kLevelSpeed = 0.2;
+    public static final Measure<Angle> kRollTolerance = Degrees.of(5.0);
+
+    public enum ClimberDirection {
+      UP,
+      DOWN
+    }
   }
 }
